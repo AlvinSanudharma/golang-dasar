8e1a5d3cec384fab49981fd8f18626c7c241e360
AlvinSanudharma <alvinsanudharma@gmail.com>
2025-12-08 20:44:51 +0800

    Slice

--- slice2/main.go
+++ slice2/main.go
@@ -8,4 +8,18 @@ func main()  {
 	fmt.Println(s);
 	fmt.Println("Length :",len(s));
 	fmt.Println("Capacity :",cap(s));
+
+	s = append(s, 10, 20)
+	fmt.Println("Setelah di append :", s)
+
+	slice2 := make([]int, 3)
+	slice3 := copy(slice2, s)
+
+	fmt.Println("Copied :", slice2)
+	fmt.Println("Jumlah elemen yang disalin :", slice3)
+
+	angka := []int{1,2,3,4,5}
+	slice4 := angka[1:4]
+
+	fmt.Println("Slice 4 :", slice4)
 }
